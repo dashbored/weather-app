85d83b7c57c934f47655b97967a637c09347a56a
dashbored <richard_davy@hotmail.com>
2019-03-26 16:20:18 +0100

    Added redux and component for weather card

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react'
 import "./Forecast.css";
 
 export default class Forecast extends Component {
+
+
   render() {
     return (
-      <div className="">
+      <div className="d-flex justify-content-around">
+        <div className="card">
+        <img className="card-img-top" src="//cdn.apixu.com/weather/64x64/day/116.png" alt="Card cap"/>
+        <div className="card-body">
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        </div>
+        <div className="card">
+        <img className="card-img-top" src="//cdn.apixu.com/weather/64x64/day/116.png" alt="Card cap"/>
+        <div className="card-body">
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        </div>
+        <div className="card">
+        <img className="card-img-top" src="//cdn.apixu.com/weather/64x64/day/116.png" alt="Card cap"/>
+        <div className="card-body">
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        </div>
+        <div className="card">
+        <img className="card-img-top" src="//cdn.apixu.com/weather/64x64/day/116.png" alt="Card cap"/>
+        <div className="card-body">
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        </div>
         <div className="card">
         <img className="card-img-top" src="//cdn.apixu.com/weather/64x64/day/116.png" alt="Card cap"/>
         <div className="card-body">
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const WeatherCard  = () => {
+    return (
+      <div>
+        
+      </div>
+    )  
+}
+
+export default WeatherCard;
